ead1ea142f34c66d993db14907a66e00f736e688
cogniera <paarths376@gmail.com>
2025-09-25 13:02:43 -0400

    revised and editied p1

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -47,19 +47,19 @@ class DetectBlue:
         # ============
         # ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
         # ============
-        
+
         # Convert BGR to HSV
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        
+
         # define range of blue color in HSV
-        lower_blue = np.array([90,150,0])
-        upper_blue = np.array([149,255,255])
-        
+        lower_blue = np.array([90, 150, 0])
+        upper_blue = np.array([149, 255, 255])
+
         # Threshold the HSV image to get only blue colors
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
-        
+
         # Bitwise-AND mask and original image
-        res = cv2.bitwise_and(img,img, mask= mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -131,7 +131,7 @@ class DetectRed:
         mask = mask1 | mask2
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img,img, mask= mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
